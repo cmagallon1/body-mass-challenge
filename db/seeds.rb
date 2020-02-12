5a22629620bc6942b4c640ba7d9b07d214becc2a
cmagallon <cesar9mv@gmail.com>
2020-02-12 16:24:30 -0600

    [update] routes and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 #
 body_mass = [
-  { category: "Very severely underweight", min: nil, max: 15 },
+  { category: "Very severely underweight", min: 0, max: 15 },
   { category: "Severely underweight", min: 15, max: 16 },
   { category: "Underweight", min: 16, max: 18.5 },
   { category: "Normal", min: 18.5, max: 25 },
@@ -17,7 +17,7 @@ body_mass = [
   { category: "Obese class III", min: 40, max: 45},
   { category: "Obese class IV", min: 45, max: 50},
   { category: "Obese class V", min: 50, max: 60},
-  { category: "Obese class VI", min: 60, max: nil }
+  { category: "Obese class VI", min: 60, max: 100 }
 ]
 
 body_mass.each { |register| BodyMass.create(register) }
