cf1ffa8a44cfd788a0412dd36a4e729c8a261947
cmagallon <cesar9mv@gmail.com>
2020-02-15 22:23:29 -0600

    [fix] styles

--- app/controllers/body_mass_categories_controller.rb
+++ app/controllers/body_mass_categories_controller.rb
@@ -6,8 +6,8 @@ class BodyMassCategoriesController < ApplicationController
   def create
     @height = body_mass_params[:height]
     @weight = body_mass_params[:weight]
-    bmi = (@weight.to_f / ((@height.to_f/100) ** 2))
-    (bmi > 0 and bmi <= 100) ? @category = BodyMass.where("min <= ? and max >= ?", bmi, bmi).first.category : @category = "You must insert valid values"
+    @bmi = (@weight.to_f / ((@height.to_f/100) ** 2))
+    (@bmi > 0 and @bmi <= 100) ? @category = BodyMass.where("min <= ? and max >= ?", @bmi, @bmi).first.category : @category = "You must insert valid values"
     respond_to do |format|
       format.html {redirect_to body_mass_categories_path, notice: "bmi calculated"}
       format.js
