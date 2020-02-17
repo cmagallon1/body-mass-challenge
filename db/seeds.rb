b033db40a229c854253c276523b10340845ed159
cmagallon <cesar9mv@gmail.com>
2020-02-17 01:06:11 -0600

    [refactor] user test and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
+BodyMass.delete_all 
+
 body_mass = [
   { category: "Very severely underweight", min: 0, max: 15 },
   { category: "Severely underweight", min: 15, max: 16 },
