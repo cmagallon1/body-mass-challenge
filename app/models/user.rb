295b2947de97784e2e017f442abd05448f3e85a2
cmagallon1 <cesar9mv@gmail.com>
2020-02-19 00:52:30 -0600

    [add]email field

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true, length: { minimum: 5, maximum: 20 }, uniqueness: true
   validates :password, length: { minimum: 5, maximum: 20 }
+  validates :email, presence: true, uniqueness: true
 end
