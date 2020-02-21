47750c19dfaf3456a486a2f8d68293501d65d501
cmagallon1 <cesar9mv@gmail.com>
2020-02-20 23:58:05 -0600

    [refactor]code

--- app/controllers/body_mass_categories_controller.rb
+++ app/controllers/body_mass_categories_controller.rb
@@ -3,34 +3,33 @@ class BodyMassCategoriesController < ApplicationController
   skip_before_action :authorized
 
   def index
-    @categoy
   end
 
   def create
-    @height = body_mass_params[:height]
-    @weight = body_mass_params[:weight]
-    @bmi = bmi(@height, @weight)
-    @category = get_category(@bmi)
-    respond_to do |format|
-      format.html {redirect_to body_mass_categories_path, notice: "bmi calculated"}
-      format.js
-    end
+    @bmi = bmi(body_params[:height], body_params[:weight])
+    @category = category(@bmi)
+    send_category(body_mass_categories_path, "bmi calculated")
   end
-
-  
 
   private 
 
-    def bmi(height, weight)
-      (@weight.to_f / ((@height.to_f/100) ** 2))
+  def send_category(path, message)
+    respond_to do |format|
+     format.html {redirect_to path, notice: message}
+     format.js
     end
+  end
 
-    def get_category(bmi)
-      (bmi > 0 && bmi <= 100) ? BodyMass.where("min <= ? and max >= ?", bmi, bmi).first.category : "You must insert valid values"
-    end
+  def bmi(height, weight)
+    (weight.to_f / ((height.to_f/100) ** 2))
+  end
 
-    def body_mass_params
-      params.require(:body_mass).permit(:height, :weight)
-    end
+  def category(bmi)
+    (bmi > 0 && bmi <= 100) ? BodyMass.where("min <= ? and max >= ?", bmi, bmi).first.category : "You must insert valid values"
+  end
+
+  def body_params
+    params.require(:body_mass).permit(:height, :weight)
+  end
 
 end
